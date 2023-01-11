309666169e8aa285c386148ca3427bccc4bea2a5
Rohan Ravindran <rohan.ravindran24@gmail.com>
2023-01-11 13:55:17 -0500

    fix EADDRINUSE error

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function (app) {
   app.use(
     ["/api", "/auth/facebook"],
     createProxyMiddleware({
-      target: "http://localhost:5000",
+      target: "http://localhost:6000",
     })
   );
 };
--- index.js
+++ index.js
@@ -39,5 +39,5 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 app.listen(port, () => console.log(`Server started on port ${port} successfully`));
